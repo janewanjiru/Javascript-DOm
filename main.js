231e8fd823c741dd8058ebe2d62aeddec5d31ab6
Jane@Wanjiru2021 <Jane@Wanjiru2021>
2021-06-14 23:55:25 +0300

    first

---
+++ main.js
@@ -0,0 +1,16 @@
+function doSomething(){
+    document.getElementById("mybody").setAttribute("style","background-color:#F5D95A;")
+ }
+ function btnFunction(){
+     var x = document.getElementById("blog").getAttribute("name")
+     document.getElementById("display").innerHTML = x
+     document.getElementById("display1").innerHTML = "'Judith - Late is not never ,its all about time'"
+     document.getElementById("display2").innerHTML = "Veronicah - Software Developer "
+     document.getElementById("display3").innerHTML = "Juliet - Life is a mistery"
+     document.getElementById("display4").innerHTML = "Jane"
+     document.getElementById("display5").innerHTML = "Pamela -The price of greatness is responsibility"
+     document.getElementById("display6").innerHTML = "Constant"
+     var y = document.getElementById("blog").hasAttribute("NAME");
+     document.getElementById("hasSomething").innerHTML = y;
+     alert("Its included")
+ }
