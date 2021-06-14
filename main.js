ab2c48e339737d985c2c9d0793115d3744664415
Jane@Wanjiru2021 <Jane@Wanjiru2021>
2021-06-15 00:21:19 +0300

    DOM HTML

--- main.js
+++ main.js
@@ -7,7 +7,7 @@ function doSomething(){
      document.getElementById("display1").innerHTML = "'Judith - Late is not never ,its all about time'"
      document.getElementById("display2").innerHTML = "Veronicah - Software Developer "
      document.getElementById("display3").innerHTML = "Juliet - Life is a mistery"
-     document.getElementById("display4").innerHTML = "Jane"
+     document.getElementById("display4").innerHTML = "Jane-if the opportunity doesn't knock , build a door. "
      document.getElementById("display5").innerHTML = "Pamela -The price of greatness is responsibility"
      document.getElementById("display6").innerHTML = "Constant"
      var y = document.getElementById("blog").hasAttribute("NAME");
